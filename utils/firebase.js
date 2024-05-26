64f32dc111ae3ae11ffe75ba9683fe3d449be193
Team RDMI <info@rdmi.in>
2024-05-26 10:50:33 +0000

    d

--- utils/firebase.js
+++ utils/firebase.js
@@ -5,14 +5,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    ,
-  aapiKey: "AIzaSyAIkDuqKxXI8xAudiygWmWXmEhnVvleJys"uthDomain: "elixcent-849aa.firebaseapp.com",
-  databaseURL: "https://elixcent-849aa-default-rtdb.firebaseio.com",
-  projectId: "elixcent-849aa",
-  storageBucket: "elixcent-849aa.appspot.com",
-  messagingSenderId: "271814045315",
-  appId: "1:271814045315:web:7b63656da3fbadad4eb5b6",
-  measurementId: "G-FXZSW520NN"
+    apiKey: "AIzaSyAIkDuqKxXI8xAudiygWmWXmEhnVvleJys",
+    authDomain: "elixcent-849aa.firebaseapp.com",
+    databaseURL: "https://elixcent-849aa-default-rtdb.firebaseio.com",
+    projectId: "elixcent-849aa",
+    storageBucket: "elixcent-849aa.appspot.com",
+    messagingSenderId: "271814045315",
+    appId: "1:271814045315:web:7b63656da3fbadad4eb5b6",
+    measurementId: "G-FXZSW520NN"
 };
 
 const app = initializeApp(firebaseConfig);
